1fb29035e5f48647bcd41c82bf879f2e2874473b
Timo Homburg <situx@users.noreply.github.com>
2026-04-19 01:41:39 +0200

    fixes

--- docs/yasguigeo.js
+++ docs/yasguigeo.js
@@ -37924,6 +37924,10 @@ void main() {
       lightingFolder.add(this.light.position, "x").min(-5).max(5).step(0.01).name("X Position");
       lightingFolder.add(this.light.position, "y").min(-5).max(5).step(0.01).name("Y Position");
       lightingFolder.add(this.light.position, "z").min(-5).max(5).step(0.01).name("Z Position");
+      const color = 16777215;
+      const intensity = 1;
+      const light = new AmbientLight(color, intensity);
+      this.scene.add(light);
       this.scene.add(this.axesHelper);
       console.log("Depth: " + (maxz - minz));
       this.scene.add(annotations);
