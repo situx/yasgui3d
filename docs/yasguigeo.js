0b41fd14128090b22ff3103431aec55037c39ae5
Timo Homburg <situx@users.noreply.github.com>
2026-04-19 00:03:02 +0200

    fixes

--- docs/yasguigeo.js
+++ docs/yasguigeo.js
@@ -32054,8 +32054,8 @@ void main() {
       console.log(verts);
       const svgShape = new Shape();
       let first = true;
-      let height = 500;
-      let width = 480;
+      let height = 600;
+      let width = 800;
       const gui = new GUI({ autoPlace: false });
       gui.domElement.id = "gui";
       document.getElementById("threejsnav").appendChild(gui.domElement);
@@ -32071,12 +32071,12 @@ void main() {
       let bbox = null;
       this.camera = new PerspectiveCamera(90, width / height, 0.1, 2e3);
       this.scene.add(new AmbientLight(2236962));
-      const light = new DirectionalLight(16777215, 1);
-      light.position.set(20, 20, 0);
-      this.scene.add(light);
-      lightingFolder.add(light.position, "x").min(-5).max(5).step(0.01).name("X Position");
-      lightingFolder.add(light.position, "y").min(-5).max(5).step(0.01).name("Y Position");
-      lightingFolder.add(light.position, "z").min(-5).max(5).step(0.01).name("Z Position");
+      this.light = new DirectionalLight(16777215, 1);
+      this.light.position.set(20, 20, 0);
+      this.scene.add(this.light);
+      lightingFolder.add(this.light.position, "x").min(-5).max(5).step(0.01).name("X Position");
+      lightingFolder.add(this.light.position, "y").min(-5).max(5).step(0.01).name("Y Position");
+      lightingFolder.add(this.light.position, "z").min(-5).max(5).step(0.01).name("Z Position");
       this.scene.add(this.axesHelper);
       console.log("Depth: " + (maxz - minz));
       this.scene.add(annotations);
@@ -32105,20 +32105,84 @@ void main() {
         );
         console.log(object3d);
         objects.add(object3d[0]);
+        this.addRotationControls(object3d, geometryF, objects, this.scene);
       }
       console.log(objects);
       this.scene.add(objects);
       gui.add(objects, "visible").name("Meshes");
       gui.add(this.axesHelper, "visible").name("Axis Helper");
+      this.fitCameraToSelection(this.camera, this.controls, objects.children);
       this.animate();
     }
     animate() {
       requestAnimationFrame(this.animatefunc);
-      console.log(this.controls);
-      console.log(this.renderer);
-      console.log(this.scene);
-      console.log(this.camera);
       this.renderer.render(this.scene, this.camera);
+    }
+    addRotationControls(box, geometryF, objects, scene) {
+      geometryF.close();
+      let yourVar = null;
+      const rotationFolder = geometryF.addFolder("Rotation");
+      rotationFolder.add(objects.rotation, "x", 0, Math.PI).name("X").onChange(
+        function() {
+          yourVar = this.getValue();
+          scene.traverse(function(obj) {
+            if (obj.type === "Mesh") {
+              obj.rotation.x = yourVar;
+            }
+          });
+        }
+      );
+      rotationFolder.add(objects.rotation, "y", 0, Math.PI).name("Y").onChange(
+        function() {
+          yourVar = this.getValue();
+          scene.traverse(function(obj) {
+            if (obj.type === "Mesh") {
+              obj.rotation.y = yourVar;
+            }
+          });
+        }
+      );
+      rotationFolder.add(objects.rotation, "z", 0, Math.PI).name("Z").onChange(
+        function() {
+          yourVar = this.getValue();
+          scene.traverse(function(obj) {
+            if (obj.type === "Mesh") {
+              obj.rotation.z = yourVar;
+            }
+          });
+        }
+      );
+      const scaleFolder = geometryF.addFolder("Scale");
+      scaleFolder.add(objects.scale, "x", 0, 2).name("X").onChange(
+        function() {
+          yourVar = this.getValue();
+          scene.traverse(function(obj) {
+            if (obj.type === "Mesh") {
+              obj.scale.x = yourVar;
+            }
+          });
+        }
+      );
+      scaleFolder.add(objects.scale, "y", 0, 2).name("Y").onChange(
+        function() {
+          yourVar = this.getValue();
+          scene.traverse(function(obj) {
+            if (obj.type === "Mesh") {
+              obj.scale.y = yourVar;
+            }
+          });
+        }
+      );
+      scaleFolder.add(objects.scale, "z", 0, 2).name("Z").onChange(
+        function() {
+          yourVar = this.getValue();
+          scene.traverse(function(obj) {
+            if (obj.type === "Mesh") {
+              obj.scale.z = yourVar;
+            }
+          });
+        }
+      );
     }
     /**
      * Build or update the Leaflet map with the current results.
